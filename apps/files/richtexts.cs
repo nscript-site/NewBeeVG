519a9e8032bb9ecb52c1a993b5a7237bcb9c543b
hufei <hufei@geblab.com>
2026-09-14 20:58:11 +0800

    修改文档

---
+++ apps/files/richtexts.cs
@@ -0,0 +1,88 @@
+#!/usr/bin/env dotnet
+
+font("阿里巴巴普惠体 2.0");
+
+var style1 = (NBVisual v) => {
+    v.As<NBTextRun>()?
+        .AddStroke(SKColors.Orange, 55)
+        .AddStroke(SKColors.White, 50)
+        .AddStroke(SKColors.Red, 40)
+        .FontSize(30).Foreground(SKColors.Black)
+        .LetterSpacing(2);
+};
+
+var style2 = (NBVisual v) => {
+    v.As<NBTextRun>()?
+        .FontSize(50)
+        .Foreground(SKColors.Blue).LetterSpacing(30);
+};
+
+var vertical = (NBVisual v) =>
+{
+    v.As<IOrientation>()?.Vertical();
+};
+
+var style3 = (NBVisual v) => {
+    v.As<NBText>()?
+        .FontSize(30).Foreground(SKColors.Black)
+        //.Align(null, 1)
+        .Align(0, 0)
+        .LetterSpacing(2)
+        .LineSpacing(10)
+        .Padding(10);
+};
+
+
+var s1 = "abcdefgdffss求关注求关注求";
+//var s1 = "abcdefgdffss求关注求关注求关注求关注求关注";
+var s2 = "求关注求关注";
+//var s1 = "ab求";
+//var s2 = "关注";
+
+// rich text clips
+var hrich = () =>
+    VStack([
+        RichText([
+            TextRun(s1).Styles(style1),
+            TextRun(s1).Styles(style2),
+            TextRun(s1).Styles(style1),
+            ]).CrossAxisAlign(0).Padding(20).LineSpacing(10).Align(0,0),
+    ]).Spacing(0).Background(SKColors.Yellow)
+    .Align(null, 0);
+
+var vrich = (bool rtl) =>
+    HStack([
+        RichText([
+            TextRun(s1).Styles(style1),
+            TextRun(s1).Styles(style2),
+            TextRun(s1).Styles(style1),
+            //TextRun(s1).Styles(style1),
+            ]).Styles(vertical).CrossAxisAlign(0).RightToLeft(rtl).Padding(20).LineSpacing(10)
+            .Align(null,0),
+    ]).Spacing(0).Background(SKColors.Yellow)
+    .Align(0, null);
+
+hrich().AsClip(out var clip01, 30, name: "hrichtext");
+vrich(false).AsClip(out var clip02, 30, name: "vrichtext");
+vrich(true).AsClip(out var clip03, 30, name: "vrichtext(rtl)");
+
+// normal text clips
+var s3 = "ABCDEDFHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890.求关注求关注求关注求关注求关注求关注求关注求关注求关注求关注求关注求关注";
+//var s3 = "ABCDED关注";
+var htext = () =>
+    VStack([
+        TextBlock(s3).Styles(style3),
+    ]).Spacing(0).Background(SKColors.Yellow)
+    .Align(null, 0);
+
+var vtext = (bool rtl) =>
+    HStack([
+        TextBlock(s3).Styles(style3, vertical).RightToLeft(rtl),
+    ]).Spacing(0).Background(SKColors.Yellow)
+    .Align(0, null);
+
+htext().AsClip(out var clip11, 30, name: "htext");
+vtext(false).AsClip(out var clip12, 30, name: "vtext");
+vtext(true).AsClip(out var clip13, 30, name: "vtext(rtl)");
+
+run(stage(1920, 1080, bg: SKColors.White), [clip01, clip02, clip03, clip11, clip12, clip13]);
---
+++ apps/files/texts.cs
@@ -0,0 +1,106 @@
+#!/usr/bin/env dotnet
+
+font("阿里巴巴普惠体 2.0");
+
+var style = (NBVisual v) => { 
+    v.As<NBText>()?
+        .AddStroke(SKColors.Orange, 55).AddStroke(SKColors.White, 50).AddStroke(SKColors.Red, 40)
+        .FontSize(80).Foreground(SKColors.Black).Align(0, 0).LetterSpacing(10).Padding(100);
+};
+
+var animate1 = (NBVisual v) =>
+{
+    v.As<NBText>()?.OnFrame(e => { e.SenderAs<NBText>()?.LetterSpacing(-80 + 100 * e.pf).InvalidateMeasure(); });
+};
+
+var vertical = (NBVisual v) =>
+{
+    v.As<NBText>()?.Vertical();
+};
+
+var content = () => 
+    VStack([
+        TextBlock("求关注").Styles(style, animate1, vertical).Id("Text").FrameMask(FrameMasks.Scanlines()),
+    ]).Spacing(0).Background(SKColors.Yellow)
+    .Align(0, 0);
+
+content().AsClip(out var clip1, 30, name: "qiuguanzhu 1");
+
+var style2 = (NBVisual v) => {
+    v.As<NBText>()?
+        .AddStroke(SKColors.Orange, 55).AddStroke(SKColors.White, 50).AddStroke(SKColors.Red, 40)
+        .FontSize(120).Foreground(SKColors.Black).Align(0, 0)
+        .OnFrame(e =>
+        {
+            var txt = e.SenderAs<NBText>()?.Text ?? "";
+            var idx = "求关注".IndexOf(txt);
+            if (idx < 0) idx = 0;
+            var frame = e.frame % 10;
+            var animateIdx = e.frame % 30 / 10;
+            if (animateIdx == idx)
+            {
+                float p = frame / 10.0f;
+                p = (float)Easing.BackIn(p);
+                e.Sender.RenderTransform(SKMatrix.CreateScale(1 + 0.2f * p, 1 + 0.2f * p));
+            }
+            else
+            {
+                e.Sender.RenderTransform();
+            }
+        });
+};
+
+var content2 =  () => 
+    VStack([
+        HStack([
+                TextBlock("求").Styles(style2),
+                TextBlock("关").Styles(style2),
+                TextBlock("注").Styles(style2),
+            ]).Spacing(20).FrameMask(FrameMasks.Scanlines()),
+    ]).Spacing(0)
+    .Align(0, 0);
+
+content2().AsClip(out var clip2, 30, name: "qiuguanzhu 2");
+
+VStack([
+    Layer([
+        TextBlock("求关注").Styles(style).ClearOnFrames().Id("Text"),
+        Rect().Bind("Text")
+            .OnFrame(e=>e.Sender.Shaders(Shaders.AlphaLinearGradient(e.p)))
+    ]).Size(double.NaN,double.NaN),
+]).Spacing(10).Align(0, 0)
+.AsClip(out var clip3, 30, name: "qiuguanzhu 3");
+
+Panel([
+    content(),
+    Ellipse(2400,2400).Align(0,0)
+        .OnFrame(e=>
+        {
+            float v = 1 - (float)Easing.CubicOut(e.p);
+            var shader = Shaders.RadialGradientOnRect([SKColors.Transparent, SKColors.Transparent, SKColors.Black, SKColors.Black],[0, v - 0.04f, v, 1]);
+            e.Sender.Shaders(shader);
+        })
+    ]).AsClip(out var clip4, 30, name: "qiuguanzhu 4");
+
+var style3 = (NBVisual v) => {
+    v.As<NBText>()?
+        .FontSize(80).Foreground(SKColors.Black).Align(0, 0)
+        .LetterSpacing(10 + 24).Padding(100);
+};
+
+var style4 = (NBVisual v) => {
+    v.As<NBText>()?
+        .FontSize(80).AddStroke(SKColors.Orange, 12).AddStroke(SKColors.Red, 10)
+        .Foreground(SKColors.Black).Align(0, 0)
+        .LetterSpacing(10).Padding(100);
+};
+
+Panel([
+    TextBlock("求关注").Styles(style3),
+    TextBlock("求关注").Styles(style4).FrameMask(FrameMasks.RectExpandMask())
+    ]).AsClip(out var clip5, 30, name: "qiuguanzhu 5");
+
+TextBlock("Demo").Foreground(SKColors.Orange).Align(1, -1).Margin(20)
+    .AsClip(out var logo, -1, 0, "logo");
+
+run(stage(1920, 1080, bg: SKColors.White), [clip1, clip2, clip3, clip4, clip5, logo]);
